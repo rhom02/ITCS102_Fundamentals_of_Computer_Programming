593a46bdb02afd79d18af9a04a3b58ca6149b643
rhom02 <rhomelynreyes388@gmail.com>
2026-09-08 21:21:56 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,19 @@
+#operators activity
+
+a = 11
+b = 8
+sum = a + b
+diff = a-b
+multiply = a*b
+division = a/b
+modulus = a%b
+exponent = a**b
+floor_div = a//b
+
+print("The sum of ",a," and", b," is", sum)
+print("The difference of ",a," minus", b," is",diff)
+print("The product of ",a," x ", b," is", a*b)
+print("The division of ",a," / ", b," is",a/b)
+print("The modulus of ",a," % ", b," is",a%b)
+print("The exponent of ",a," ** ", b," is", a**b)
+print("The floor_div of ",a," // ", b," is", a//b)
