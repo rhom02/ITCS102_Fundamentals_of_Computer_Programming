642b2163c73c5d7ea462fb9f6cf679d39616ae0e
rhom02 <rhomelynreyes388@gmail.com>
2026-09-08 22:08:46 +0800

    Add files via upload

---
+++ activity 9.py
@@ -0,0 +1,21 @@
+#relational operator activity
+
+x = 11
+y = 8
+
+password1 = "sample"
+password2 = "sample"
+
+#1
+print(x > y)
+#2
+print(x < y)
+#3
+print(x >= y)
+#4
+print(x <= y)
+#5 ==
+print(x== y)
+print(password1 == password2)
+#6 !=
+print(password1 != password2)
