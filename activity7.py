47873b5c685de629055d1d7b0e43439200cc83bf
rhom02 <rhomelynreyes388@gmail.com>
2026-09-08 21:38:41 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,30 @@
+total = 8
+
+r = eval(input("enter a number ---> "))
+total = total + r
+h = eval(input("enter a number ---> "))
+total += h
+o = eval(input("enter a number ---> "))
+total += o
+m = eval(input("enter a number ---> "))
+total += m
+e = eval(input("enter a number ---> "))
+total -= e
+l = eval(input("enter a number ---> "))
+total -= l
+y = eval(input("enter a number ---> "))
+total *= y
+n = eval(input("enter a number ---> "))
+total /= n
+
+print("Current Total is --> ", total)
+
+
+r = 10
+h = 10
+o = 10
+m = 10
+e = 4
+l = 4
+y = 2
+n = 2
