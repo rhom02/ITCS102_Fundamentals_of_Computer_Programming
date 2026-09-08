d398221ee1fd29af20b29652de3b502ce3db5229
rhom02 <rhomelynreyes388@gmail.com>
2026-09-08 22:49:55 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,11 @@
+#escape sequence
+#\n - new line
+#\t - tab space
+#\r - carriage return / depracated (obsolute)
+#\b - back space
+#\\ - insert slash
+#\" - insert double quotation
+
+phrase1 = "\t\tOne day at \r\"a time, \n\t\t\t\tand that's all \\i need\" ,,yiezzz \n\t\tyou \b\b\b need it"
+
+print(phrase1)
