a23e8bbe5d8a9252b5a8841a3705fd8db2fb5fa8
rhom02 <rhomelynreyes388@gmail.com>
2026-09-08 22:24:39 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,11 @@
+#basic if else program
+
+username = "minii"
+password = "rhom02"
+
+u = input("Input USERNAME ---> ")
+p = input("Input PASSWORD ---> ")
+
+if u == username and p == password:
+        print("username and password correct")
+else:   print("access denied")
